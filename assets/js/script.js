b799a4c18f7adc549c990542faeed5918a185eb0
[Andrew Core] <[adcore89@gmail.com]>
2021-03-08 21:55:43 -0500

    form function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
 var createTaskHandler = function(event) { 
-  event.preventDefault(); 
+  event.preventDefault();
+  
+  var taskNameInput = document.querySelector("input[name='task-name']");
+  console.dir(taskNameInput);
 
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
+  listItemEl.textContent = "This is a new task"; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
